9b4ecb7768967d27ae7de2eb07cf0b31313d1f96
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-03-21 18:35:01 -0400

    fleshing out login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,47 @@
-import './Login.css'
+import { useState } from 'react';
+import './Login.css';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className='btn'>login</button>
+      {/* {!isPending && <button className='btn'>Login</button>}
+      {isPending && (
+        <button className='btn' disabled>
+          loading
+        </button>
+      )}
+      {error && <div className='error'>{error}</div>} */}
+    </form>
+  );
+};
+export default Login;
