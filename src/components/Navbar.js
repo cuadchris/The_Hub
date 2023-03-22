623f53e034b3e97c524e613e79cc7e8b20e2ba53
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-03-22 14:50:43 -0400

    user avatar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import './Navbar.css';
 import Temple from '../assets/temple.svg';
 import { Link } from 'react-router-dom';
-import { useLogout } from '../hooks/useLogout'
+import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const Navbar = () => {
-
   const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <div className='navbar'>
@@ -14,17 +15,29 @@ const Navbar = () => {
           <img src={Temple} alt='hub logo' />
           <span>The Hub</span>
         </li>
-
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Sign up</Link>
-        </li>
-        <li>
-          {!isPending && <button className='btn' onClick={logout}>Logout</button>}
-          {isPending && <button className='btn' disabled>Logging out...</button>}
-        </li>
+        {user ? (
+          <li>
+            {!isPending && (
+              <button className='btn' onClick={logout}>
+                Logout
+              </button>
+            )}
+            {isPending && (
+              <button className='btn' disabled>
+                Logging out...
+              </button>
+            )}
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>Sign up</Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,14 +2,18 @@ import './Sidebar.css';
 import { NavLink } from 'react-router-dom';
 import DashboardIcon from '../assets/dashboard_icon.svg';
 import AddIcon from '../assets/add_icon.svg';
+import Avatar from './Avatar';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const Sidebar = () => {
+  const { user } = useAuthContext();
+
   return (
     <div className='sidebar'>
       <div className='sidebar-content'>
         <div className='user'>
-          {/* avatar and username here later on */}
-          <p>Howdy user</p>
+          <Avatar src={user.photoURL} />
+          <p>Howdy {user.displayName}</p>
         </div>
         <nav className='links'>
           <ul>
