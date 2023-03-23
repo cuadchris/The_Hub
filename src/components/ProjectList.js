9e54e9d479a8973b707a3be0e4f992dfaf8c46dd
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-03-23 11:49:48 -0400

    more work on project list component

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,28 @@
+import { Link } from 'react-router-dom';
+import Avatar from './Avatar';
 import './ProjectList.css';
 
 const ProjectList = ({ projects }) => {
   return (
-    <div>
+    <div className='project-list'>
       {projects.length === 0 ? (
         <p>No projects yet</p>
       ) : (
-        projects.map((project) => <div key={project.id}>{project.name}</div>)
+        projects.map((project) => (
+          <Link to={`/project/${project.id}`} key={project.id}>
+            <h4>{project.name}</h4>
+            <p>due by {project.dueDate.toDate().toDateString()}</p>
+            <div className='assigned-to'>
+              <ul>
+                {project.assignedUsersList.map((user) => (
+                  <li key={user.photoURL}>
+                    <Avatar src={user.photoURL} />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Link>
+        ))
       )}
     </div>
   );
